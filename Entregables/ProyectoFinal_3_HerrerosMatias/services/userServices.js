0ac4dc3ef8a29a36150467b9111cd9a551ab2421
Matias Herreros <matiasherreros97@gmail.com>
2022-09-07 21:38:37 -0300

    IMPLEMENTED arquitectura usuarios 80%

---
+++ Entregables/ProyectoFinal_3_HerrerosMatias/services/userServices.js
@@ -0,0 +1,8 @@
+const getStorage = require('../db/daos')
+const { users: storage } = getStorage()
+
+const getItemById = async(itemId) => {
+    return await storage.getById((itemId))
+}
+
+module.exports = { getItemById }
